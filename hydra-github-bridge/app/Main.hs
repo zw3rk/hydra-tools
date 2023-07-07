44797eb9aa3b190d694e2622dc4e7fa5cf158b3b
Robin Stumm <robin.stumm@iohk.io>
2023-07-07 21:00:28 +0200

    hydra-github-bridge: report build status in description

--- hydra-github-bridge/app/Main.hs
+++ hydra-github-bridge/app/Main.hs
@@ -57,6 +57,60 @@ data HydraNotification
     | BuildFinished BuildId
     deriving (Show, Eq)
 
+data BuildStatus
+    = Succeeded
+    | Failed
+    | DependencyFailed
+    | Aborted
+    | Cancelled
+    | FailedWithOutput
+    | TimedOut
+    | LogLimitExceeded
+    | OutputSizeLimitExceeded
+    | NonDeterministicBuild
+    | Other
+
+instance Show BuildStatus where
+    show s = case s of
+        (Succeeded)               -> "Build succeeded."
+        (Failed)                  -> "Build failed."
+        (DependencyFailed)        -> "Build dependency failed."
+        (Aborted)                 -> "Build aborted."
+        (Cancelled)               -> "Build cancelled."
+        (FailedWithOutput)        -> "Build failed with output."
+        (TimedOut)                -> "Build timed out."
+        (LogLimitExceeded)        -> "Build log limit exceeded."
+        (OutputSizeLimitExceeded) -> "Build output size limit exceeded."
+        (NonDeterministicBuild)   -> "Build is non-deterministic."
+        (Other)                   -> "Build failed due to unknown reason."
+
+instance Enum BuildStatus where
+    toEnum i = case i of
+        ( 0) -> Succeeded
+        ( 1) -> Failed
+        ( 2) -> DependencyFailed
+        ( 3) -> Aborted
+        ( 9) -> Aborted
+        ( 4) -> Cancelled
+        ( 6) -> FailedWithOutput
+        ( 7) -> TimedOut
+        (10) -> LogLimitExceeded
+        (11) -> OutputSizeLimitExceeded
+        (12) -> NonDeterministicBuild
+        ( _) -> Other
+    fromEnum i = case i of
+        (Succeeded)               ->  0
+        (Failed)                  ->  1
+        (DependencyFailed)        ->  2
+        (Aborted)                 ->  3
+        (Cancelled)               ->  4
+        (FailedWithOutput)        ->  6
+        (TimedOut)                ->  7
+        (LogLimitExceeded)        -> 10
+        (OutputSizeLimitExceeded) -> 11
+        (NonDeterministicBuild)   -> 12
+        (Other)                   -> 99
+
 data StatusState = Error | Failure | Pending | Success
     deriving (Show, Eq, Generic)
 
@@ -97,6 +151,20 @@ instance ToJSON GitHubStatus where
 
 instance FromJSON GitHubStatus where
   parseJSON = genericParseJSON $ aesonDrop 0 camelCase
+
+toStatusState :: BuildStatus -> StatusState
+toStatusState b = case b of
+    (Succeeded)               -> Success
+    (Failed)                  -> Failure
+    (DependencyFailed)        -> Failure
+    (Aborted)                 -> Error
+    (Cancelled)               -> Error
+    (FailedWithOutput)        -> Failure
+    (TimedOut)                -> Failure
+    (LogLimitExceeded)        -> Error
+    (OutputSizeLimitExceeded) -> Error
+    (NonDeterministicBuild)   -> Failure
+    (Other)                   -> Failure
 
 -- Text utils
 tshow :: Show a => a -> Text
@@ -169,10 +237,11 @@ handleHydraNotification conn host e = flip catch (handler e) $ case e of
     (BuildFinished bid) -> do
         [(proj, name, flake, job, desc, finished, status)] <- query conn ("select j.project, j.name, e.flake, b.job, b.description, b.finished, b.buildstatus" <> sqlFromBuild) (Only bid)
         Text.putStrLn $ "Build Finished (" <> tshow bid <> "): " <> (proj :: Text) <> ":" <> (name :: Text) <> " " <> (job :: Text) <> "(" <> maybe "" id (desc :: Maybe Text) <> ")" <> " " <> tshow (parseGitHubFlakeURI flake)
-        let ghStatus | (finished, status) == ((1, 0) :: (Int, Int)) = Success
-                     | otherwise   = Failure
+        let buildStatus = toEnum status
+        let ghStatus | finished == (1 :: Int) = toStatusState buildStatus
+                     | otherwise              = Failure
         whenStatusOrJob (Just ghStatus) job $ withGithubFlake flake $ \owner repo hash ->
-            pure $ singleton (GitHubStatus owner repo hash (GitHubStatusPayload ghStatus {- target url: -} ("https://" <> host <> "/build/" <> tshow bid) {- description: -} (Just "Build Finished.") ("ci/hydra-build:" <> job)))
+            pure $ singleton (GitHubStatus owner repo hash (GitHubStatusPayload ghStatus {- target url: -} ("https://" <> host <> "/build/" <> tshow bid) {- description: -} (Just $ tshow buildStatus) ("ci/hydra-build:" <> job)))
 
     -- _ -> print e >> pure []
 
@@ -245,13 +314,13 @@ handleHydraNotification conn host e = flip catch (handler e) $ case e of
                 \ " [eid, jid, eid] :: IO [(Int, Text, Int)]
                 buildStatuses <- sequence $ map
                     (\(bid, job, status) -> do
-                        let ghStatus | status == (0 :: Int) = Success
-                                     | otherwise            = Failure
+                        let buildStatus = toEnum status
+                        let ghStatus = toStatusState buildStatus
                         whenStatusOrJob (Just ghStatus) job $
                             pure $ singleton $ GitHubStatus owner repo hash $
                                 GitHubStatusPayload ghStatus
                                     {- target url: -} ("https://" <> host <> "/build/" <> tshow bid)
-                                    {- description: -} (Just "Build Finished.")
+                                    {- description: -} (Just $ tshow buildStatus)
                                     ("ci/hydra-build:" <> job))
                     rows
                 pure $ evalStatuses ++ concat buildStatuses
