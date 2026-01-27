b94bba57cd02ec3b49fd4b200ae3f93112d015ec
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2026-01-28 10:49:05 +1300

    Include triggers for jobs with no steps (#24)
    If a jobs derivation was built outside of hydra before it is included as
    a job, it does not need any build steps to complete.
    
    `hydra-attic-bridge` does not upload the to attic (because it only
    uploads the derivations and outputs of build steps).
    
    This change adds triggers to the `Builds` and `BuildOutputs` tables to
    like those on `BuildSteps` and `BuildStepOutputs`, so that these
    derivations and their outputs are not missed.

--- hydra-attic-bridge/setup.sql
+++ hydra-attic-bridge/setup.sql
@@ -41,3 +41,42 @@ AFTER INSERT OR UPDATE ON BuildStepOutputs
 FOR EACH ROW
 WHEN (NEW.path is not NULL)
 EXECUTE PROCEDURE process_build_step_output();
+
+-- Just in cast no build steps were needed.
+-- This can happen if the derivation was built by something
+-- other than hydra already.
+CREATE OR REPLACE FUNCTION process_build_update()
+RETURNS trigger AS $$
+BEGIN
+  -- Check if buildstatus has been updated to 0
+  IF NEW.buildstatus = 0 AND OLD.buildstatus is NULL THEN
+      -- Insert the drvpath into the queue table
+      INSERT INTO DrvpathsToUpload (drvpath) VALUES (NEW.drvpath);
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER build_update_trigger
+AFTER UPDATE ON Builds
+FOR EACH ROW
+WHEN (NEW.buildstatus = 0 AND OLD.buildstatus is NULL)
+EXECUTE PROCEDURE process_build_update();
+
+CREATE OR REPLACE FUNCTION process_build_output()
+RETURNS trigger AS $$
+BEGIN
+  -- Check if status has been updated to 0
+  IF NEW.path is not NULL THEN
+      -- Insert the (drv)path into the queue table
+      INSERT INTO DrvpathsToUpload (drvpath) VALUES (NEW.path);
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER build_output_trigger
+AFTER INSERT OR UPDATE ON BuildOutputs
+FOR EACH ROW
+WHEN (NEW.path is not NULL)
+EXECUTE PROCEDURE process_build_output();
