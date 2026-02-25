389b97435a8ad38397ee774ba7803e556c675bb0
Sean D Gillespie <sean.gillespie@iohk.io>
2026-02-25 10:19:41 -0500

    Don't crash after fetching unknown app installations
    Previously, we terminated the server with `error`. To make matters
    worse, we only did this check when refreshing the tokens (not on
    startup), so this condition was only triggered after refetching the
    tokens.
    
    As a result,
    
     1. We could be building unknown codebases
     2. The server crashes once per hour

--- hydra-github-bridge/app/Main.hs
+++ hydra-github-bridge/app/Main.hs
@@ -33,46 +33,82 @@ import System.IO
     stdin,
     stdout,
   )
+import Data.Maybe (fromMaybe)
 
-fetchGitHubTokens :: Int -> FilePath -> Text -> ByteString -> IO [(String, TokenLease)]
-fetchGitHubTokens ghAppId ghAppKeyFile ghEndpointUrl ghUserAgent = do
+fetchGitHubTokens ::
+  Int ->
+  FilePath ->
+  Text ->
+  ByteString ->
+  [(Text, Int)] ->
+  IO [(String, TokenLease)]
+fetchGitHubTokens ghAppId ghAppKeyFile ghEndpointUrl ghUserAgent ghAppInstallIds = do
+  -- Fetch installations
   putStrLn "Fetching GitHub App installations..."
   ghAppInstalls <- fetchInstallations ghEndpointUrl ghAppId ghAppKeyFile ghUserAgent
   putStrLn $ "Found " <> show (length ghAppInstalls) <> " installations"
   forM_ ghAppInstalls $ \(owner, installId) -> do
     Text.putStrLn $ "\t- " <> owner <> " (" <> Text.pack (show installId) <> ")"
 
-  forM ghAppInstalls $ \(owner, installId) -> do
+  -- Filter out installations not configured
+  appInstalls <- flip filterM ghAppInstalls $ \inst@(owner, installId) -> do
+    let found = inst `elem` ghAppInstallIds
+    unless found $
+      Text.putStrLn $
+        "Warning: No configured GitHub App Installation ID: "
+          <> owner
+          <> " ("
+          <> Text.show installId
+          <> ")"
+
+    pure True
+
+  -- Fetch app installation tokens
+  forM appInstalls $ \(owner, installId) -> do
     lease <- fetchAppInstallationToken ghEndpointUrl ghAppId ghAppKeyFile ghUserAgent installId
     Text.putStrLn $ "Fetched new GitHub App installation token valid for " <> owner <> " until " <> Text.pack (show lease.expiry)
     return (Text.unpack owner, lease)
 
 getValidGitHubToken ::
-  IORef [(String, TokenLease)] ->
+  Int ->
+  FilePath ->
   Text ->
   ByteString ->
+  [(Text, Int)] ->
+  IORef [(String, TokenLease)] ->
   IO [(String, TokenLease)]
-getValidGitHubToken ghTokens ghEndpointUrl ghUserAgent =
+getValidGitHubToken ghAppId ghAppKeyFile ghEndpointUrl ghUserAgent ghAppInstallIds ghTokens =
+  -- Fetch tokens from in-memory state. If they are set to expire within 5 seconds,
+  -- refresh them from GitHub.
   let buffer = 5 :: NominalDiffTime
    in getValidToken buffer ghTokens $ \owner -> do
         putStrLn $ "GitHub token expired or will expire within the next " <> show buffer <> ", fetching a new one..."
-        ghAppId <- getEnv "GITHUB_APP_ID" >>= return . read
-        ghAppInstallIds <- getEnv "GITHUB_APP_INSTALL_IDS" >>= return . read @[(String, Int)]
-        let ghAppInstallId = fmap snd . find ((owner ==) . fst) $ ghAppInstallIds
-        ghAppKeyFile <- getEnv "GITHUB_APP_KEY_FILE"
-        maybe
-          (error $ "No configured GitHub App Installation ID " <> owner)
-          (fetchAppInstallationToken ghEndpointUrl ghAppId ghAppKeyFile ghUserAgent)
-          ghAppInstallId
 
-getValidToken :: NominalDiffTime -> IORef [(String, TokenLease)] -> (String -> IO TokenLease) -> IO [(String, TokenLease)]
+        -- Lookup the installation for the owner in the configured App Installation IDs.
+        -- If found, fetch the token. If we don't know about it, we don't want to use it
+        -- (eg, if a stranger found and installed our app).
+        let ghAppInstallId = fmap snd . find ((owner ==) . Text.unpack . fst) $ ghAppInstallIds
+        case ghAppInstallId of
+          Nothing -> do
+            Text.putStrLn $ "Warning: No configured GitHub App Installation ID " <> Text.pack owner
+            pure Nothing
+          Just inst ->
+            Just <$> fetchAppInstallationToken ghEndpointUrl ghAppId ghAppKeyFile ghUserAgent inst
+
+-- | Look up tokens from in-memory state. If they are set to expire within 'buffer', run
+-- the 'fetch' action.
+getValidToken ::
+  NominalDiffTime ->
+  IORef [(String, TokenLease)] ->
+  (String -> IO (Maybe TokenLease)) ->
+  IO [(String, TokenLease)]
 getValidToken buffer lease fetch = do
   leases' <- readIORef lease
   now <- getCurrentTime
-  leases'' <- forM leases' $ \(owner, tok) -> do
+  leases'' <- forM leases' $ \lease'@(owner, tok) -> do
     case tok.expiry of
       Just expiry' | addUTCTime buffer now < expiry' -> return (owner, tok)
-      _ -> (owner,) <$> fetch owner
+      _ -> maybe lease' (owner,) <$> fetch owner
   writeIORef lease leases''
   return leases''
 
@@ -83,23 +119,25 @@ main = do
   hSetBuffering stderr LineBuffering
 
   -- Read environment variables
-  host <- maybe "localhost" id <$> lookupEnv "HYDRA_HOST"
-  db <- maybe "localhost" id <$> lookupEnv "HYDRA_DB"
-  db_user <- maybe mempty id <$> lookupEnv "HYDRA_DB_USER"
-  db_pass <- maybe mempty id <$> lookupEnv "HYDRA_DB_PASS"
+  host <- fromMaybe "localhost" <$> lookupEnv "HYDRA_HOST"
+  db <- fromMaybe "localhost" <$> lookupEnv "HYDRA_DB"
+  db_user <- fromMaybe mempty <$> lookupEnv "HYDRA_DB_USER"
+  db_pass <- fromMaybe mempty <$> lookupEnv "HYDRA_DB_PASS"
   api_user <- maybe mempty Text.pack <$> lookupEnv "HYDRA_USER"
   api_pass <- maybe mempty Text.pack <$> lookupEnv "HYDRA_PASS"
   port <- maybe 8080 read <$> lookupEnv "PORT"
   stateDir <- getEnv "HYDRA_STATE_DIR"
-  ghEndpointUrl <- Text.pack . maybe "https://api.github.com" id <$> lookupEnv "GITHUB_ENDPOINT_URL"
+  ghEndpointUrl <- maybe "https://api.github.com" cs <$> lookupEnv "GITHUB_ENDPOINT_URL"
   ghUserAgent <- maybe "hydra-github-bridge" cs <$> lookupEnv "GITHUB_USER_AGENT"
   ghKey <- maybe mempty C8.pack <$> lookupEnv "GITHUB_WEBHOOK_SECRET"
 
   -- Authenticate to GitHub
-  ghAppId <- getEnv "GITHUB_APP_ID" >>= return . read
+  ghAppId <- read <$> getEnv "GITHUB_APP_ID"
   ghAppKeyFile <- getEnv "GITHUB_APP_KEY_FILE"
+  ghAppInstallIds <- read <$> getEnv "GITHUB_APP_INSTALL_IDS"
   -- ghTokens is basically [(String, Token)]
-  ghTokens <- fetchGitHubTokens ghAppId ghAppKeyFile ghEndpointUrl ghUserAgent >>= newIORef
+  ghTokens <- fetchGitHubTokens ghAppId ghAppKeyFile ghEndpointUrl ghUserAgent ghAppInstallIds
+  ghTokensRef <- newIORef ghTokens
 
   putStrLn $ "Connecting to Hydra at " <> host
   env <- hydraClientEnv (Text.pack host) api_user api_pass
@@ -108,7 +146,14 @@ main = do
 
   -- Start the app loop
   let numWorkers = 10 -- default number of workers
-      getValidGitHubToken' = getValidGitHubToken ghTokens ghEndpointUrl ghUserAgent
+      getValidGitHubToken' =
+        getValidGitHubToken
+          ghAppId
+          ghAppKeyFile
+          ghEndpointUrl
+          ghUserAgent
+          ghAppInstallIds
+          ghTokensRef
 
   Async.mapConcurrently_
     id
